e0fe190c1a83143527ae1fc439cd2cc8b995bfbc
Quadrosh <quadrosh@gmail.com>
2016-11-12 11:40:30 +0300

    search

--- config/web.php
+++ config/web.php
@@ -52,6 +52,7 @@ $config = [
                 'category/<id:\d+>/<page:\d+>' => 'category/view',
                 'category/<id:\d+>' => 'category/view',
                 'product/<id:\d+>' => 'product/view',
+                'search' => 'category/search',
             ],
         ],
      //  'assetManager' => [
